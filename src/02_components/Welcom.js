de585050e06047863e2c91cdf28397c2f87913c5
TatsianaMark <tanya030890@mail.ru>
2019-11-04 19:27:45 +0300

    create component

---
+++ src/02_components/Welcom.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+// function Welcome() {
+//     return <h1>Hello world!</h1>
+// }
+//
+// export default Welcome;
+
+class Welcom extends Component{
+    render() {
+        return <h1>Hello world!</h1>
+    }
+}
+export default Welcom;
