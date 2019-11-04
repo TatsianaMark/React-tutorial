6f517690ef113a749f3b77e064fae3bdec7c57b5
TatsianaMark <tanya030890@mail.ru>
2019-11-04 20:19:47 +0300

    added CounterButton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Welcom from "./02_components/Welcom";
+import CounterButton from "./03_state/CounterButton";
 
 
 function App(){
     return(
         <div>
-           <Welcom/>
+           <CounterButton/>
         </div>
     )
 }
