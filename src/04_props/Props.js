4eb49c5525d8acbe59a6f0d46da3b7fa7b8a4ea2
TatsianaMark <tanya030890@mail.ru>
2019-11-04 20:43:16 +0300

    using props

---
+++ src/04_props/Props.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+const Counter = (props) =>(
+    console.log(props));
+return  <h1>{`Counter component. Counter value is: ${ props.counter}`}</h1>
+)
+
+
+class Props extends Component{
+    state = {
+        counter:0,
+    }
+
+    handleClick = ()=> {
+        this.setState(({ counter }) =>({
+            counter: ++counter,
+        }))
+    }
+
+    render(){
+        const { counter } = this.state;
+
+        return(
+            <div>
+                <div>{counter}</div>
+                <Counter counter={counter}/>
+                <button onClick={this.handleClick}>+1</button>
+            </div>
+        );
+    }
+}
+
+export default Props;
--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Welcom from "./02_components/Welcom";
 import CounterButton from "./03_state/CounterButton";
+import Props from "./04_props/Props";
 
 
 function App(){
     return(
         <div>
-           <CounterButton/>
+           <Props/>
         </div>
     )
 }
